6f0caac8cff6027cab727a073a8a2fde0c7a010e
Arup Jana <arup@vbox.myguest.virtualbox.org>
2025-07-10 14:47:11 +0530

    setup testing

---
+++ Makefile
@@ -0,0 +1,2 @@
+builds/main: src/main.c
+	gcc src/main.c -o builds/main
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+	char db_name[128];
+	strcpy(db_name, "tinydb V1.0");
+
+	printf("%s\n", db_name);
+
+	return 0;
+}
